8b9db430bc3fad017756a9ddbaa06d9a9529f701
Genghis Khan <gkomet@g.jct.ac.il>
2020-03-26 10:02:34 +0200

    Corrected version

--- Detector.py
+++ Detector.py
@@ -2,39 +2,29 @@ from scapy.all import *
 from argparse import ArgumentParser
 
 
+hw_addresses = []
+p_addresses = []
+
+
 def arp_filter(packet):
     return ARP in packet and packet.op == 2  # is-at
 
 
-def ret_hw_src_address(packet):
-    return packet.hwsrc
-
-
-def ret_p_src_address(packet):
-    return packet.psrc
-
-
-def has_duplicates(packets):
-    hw_src_address_list = map(ret_hw_src_address,packets)
-    p_src_address_list = map(ret_p_src_address, packets)
-
-    for index in range(len(packets)):
-        if hw_src_address_list[index] in hw_src_address_list[:index] + hw_src_address_list[index + 1:]\
-        and p_src_address_list[index] in p_src_address_list[:index] + p_src_address_list[index + 1:]:
-            return True
-
-    return False
-
+def check_for_dup(packet):
+    if hw_addresses.index(packet[ARP].hwsrc) == p_addresses.index(packet[ARP].psrc):
+        raise ValueError
+    hw_addresses.append(packet[ARP].hwsrc)
+    p_addresses.append(packet[ARP].psrc)
 
 
 def main():
-    parser = ArgumentParser(description="Detects if an ARP poisoning attack is occuring:")
-    parser.add_argument("-t", "--time", default=60, type=int, help="Time to wait until check for duplicate ARP is-at messages")
+    parser = ArgumentParser(description="Detects if an ARP poisoning attack is occuring")
+    parser.add_argument("-t", "--time", default=20, type=int, help="Time to wait until check for duplicate ARP is-at messages")
     args = parser.parse_args()
 
-    packets = sniff(timeout=10, lfilter=arp_filter)
-
-    if has_duplicates(packets):
+    try:
+        packets = sniff(timeout=args.time, lfilter=arp_filter, prn=check_for_dup)
+    except ValueError:
         print("You are being poisoned!")
 
 
